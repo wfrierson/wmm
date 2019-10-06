b7b032a58bb69a7e7b434356eaf4197aaed7f8a6
Will Frierson <will.frierson@gmail.com>
2019-10-05 23:53:06 -0700

    master: Adding namespaces and comments.

--- data-raw/creatingSysData.R
+++ data-raw/creatingSysData.R
@@ -2,7 +2,7 @@
 ## The following objects are built and then saved in R/sysdata.rda
 
 ###############################################################################
-## Constants
+## Define constants
 
 # Flattening constant
 .f <- 1 / 298.257223563
@@ -30,6 +30,9 @@
 # https://www.ngdc.noaa.gov/geomag/WMM/data/WMM2010/WMM2010COF.zip
 # https://www.ngdc.noaa.gov/geomag/WMM/data/WMM2015/WMM2015COF.zip
 # https://www.ngdc.noaa.gov/geomag/WMM/data/WMM2015/WMM2015v2COF.zip
+#
+# The following files are in the extdata folder and are formatted versions of
+# the WMM coefficients.
 .filenames.wmm <- c(
   'WMM2005.csv',
   'WMM2010.csv',
@@ -48,13 +51,15 @@
     function(path) {
       versionWMM <- tools::file_path_sans_ext(basename(path))
 
-      data.table::fread(
+      output <- data.table::fread(
         path,
         sep = '|',
         header = TRUE
       )[
         , version := (versionWMM)
       ]
+
+      return(output)
     }
   ),
   use.names = TRUE
@@ -68,14 +73,14 @@ data.table::setkey(
 # Note nDegree = 13 needed to calculate P_Schmidt_muDeriv, even though only the
 # first 12 degrees are summed.
 .P <- data.table::data.table(n = 1:13, key = "n")
-.P <- .P[, .(m = 0:n), by = key(.P)]
+.P <- .P[, .(m = 0:n), by = eval(data.table::key(.P))]
 data.table::setkey(.P, n, m)
 
 # Define index used for recursion, see P_recursive for details
 .P[, index := .I]
 
 # index == 6 means the constant m recursion relation is used
-.P[.P[!J(0:2)][CJ(unique(n), 0:1)], index := 6]
+.P[.P[!J(0:2)][data.table::CJ(unique(n), 0:1)], index := 6]
 
 # index == 7 means the constant n recursion relation is used
 .P[index > 6, index := 7]

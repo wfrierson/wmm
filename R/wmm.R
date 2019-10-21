accd568546930829a7f3f15bb844986713645510
Will Frierson <will.frierson@gmail.com>
2019-10-20 18:26:18 -0700

    improving_speed: Adding package-level help file.

---
+++ R/wmm.R
@@ -0,0 +1,13 @@
+.datatable.aware = TRUE
+
+#' wmm: R Implementation of World Magnetic Model
+#'
+#' The \code{wmm} package calculates magnetic field at a given location and time according to the World Magnetic Model.
+#'
+#' @section WMM functions:
+#' This package has 1 exported function: \code{\link{GetMagneticFieldWMM}}
+#'
+#' @docType package
+#' @name wmm
+
+NULL
